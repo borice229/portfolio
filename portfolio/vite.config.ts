d33e3f608559bf2ac8082652d1ad3f26756a78dd
borice229 <boricefiacredossou@gmail.com>
2026-02-03 00:59:25 +0100

    Ajout officiel de la version anglaise

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/portfolio/', // Indispensable pour GitHub Pages
+  base: '/portfolio/', // Garde bien le nom de ton dépôt ici
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        en: resolve(__dirname, 'en.html'),
+      },
+    },
+  },
 })
