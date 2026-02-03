4f1d913fb434192399e681b3e62908d972ebca3a
borice229 <boricefiacredossou@gmail.com>
2026-02-03 01:38:12 +0100

    Correction des chemins CSS et liens HTML

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/portfolio/', // Garde bien le nom de ton dépôt ici
+  base: './', // Utiliser des chemins relatifs pour que ça marche partout
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        en: resolve(__dirname, 'en.html'),
+        main: './index.html',
+        en: './en.html',
       },
     },
   },
